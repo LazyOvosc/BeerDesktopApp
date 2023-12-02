9f75443938cedf6b5d0cb31774be840ff564ace5
teresabacho <terkabacho@gmail.com>
2023-12-02 13:19:12 +0200

    Add dbcontext

---
+++ WPFSipBiteUnite/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using WPFSipBiteUnite.DataBaseClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace WPFSipBiteUnite.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(): base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=postgres;Password=1111;Database=DbSipBiteUnite;");
+            }
+        }
+
+    }
+}
