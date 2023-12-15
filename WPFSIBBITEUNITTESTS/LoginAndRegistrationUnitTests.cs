40f561f7506274c3a63a7069a00e1de81eb5201c
GunGaX <dmytro.fwork@gmail.com>
2023-12-15 13:30:06 +0200

    Registration Unit Tests

---
+++ WPFSIBBITEUNITTESTS/LoginAndRegistrationUnitTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using WPFSipBiteUnite.ViewModel;
+
+namespace WPFSipBiteUniteTest
+{
+    [TestClass]
+    public class RegistrationViewModelTests
+    {
+        [TestMethod]
+        public void CanExecuteRegisterCommand_ValidEmailAndPassword_ReturnsTrue()
+        {
+            // Arrange
+            var viewModel = new RegistrationViewModel
+            {
+                Username = "test@example.com",
+                Password = "Password123"
+            };
+
+            // Act
+            var result = viewModel.CanExecuteRegisterCommand(null);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void CanExecuteRegisterCommand_InvalidEmail_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = new RegistrationViewModel
+            {
+                Username = "invalid_email",
+                Password = "Password123"
+            };
+
+            // Act
+            var result = viewModel.CanExecuteRegisterCommand(null);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CanExecuteRegisterCommand_InvalidPassword_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = new RegistrationViewModel
+            {
+                Username = "test@example.com",
+                Password = "short"
+            };
+
+            // Act
+            var result = viewModel.CanExecuteRegisterCommand(null);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CanExecuteRegisterCommand_NullUsername_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = new RegistrationViewModel
+            {
+                Username = null,
+                Password = "Password123"
+            };
+
+            // Act
+            var result = viewModel.CanExecuteRegisterCommand(null);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CanExecuteRegisterCommand_NullPassword_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = new RegistrationViewModel
+            {
+                Username = "test@example.com",
+                Password = null
+            };
+
+            // Act
+            var result = viewModel.CanExecuteRegisterCommand(null);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
